6de3f563e20c20cf7d658d68896ec4e3c87fb057
Tanner Nielsen <tan-ali5@outlook.com>
2020-08-13 21:59:58 -0700

    updates

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect( process.envMONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
