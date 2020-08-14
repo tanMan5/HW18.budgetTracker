a84852de0bff52ed69a5d4fc8d2558b6a8e373ef
Tanner Nielsen <tan-ali5@outlook.com>
2020-08-13 22:09:19 -0700

    fixed icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
     "/db.js",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
-    "/icons/icon-192x192.png"
+    "/icons/icon-512x512.png"
   ];
   
   const CACHE_NAME = "static-cache-v2";
