95ac7fb4c28c0682190a549a0b88b21771270c48
Tanner Nielsen <tan-ali5@outlook.com>
2020-08-13 22:23:11 -0700

    fixed syntax/spacing

--- public/db.js
+++ public/db.js
@@ -36,20 +36,20 @@ function checkDatabase() {
 
     getAll.onsuccess = function() {
         if (getAll.result.length > 0) {
-            fetch("/api/transaction/bulk", {
+          fetch("/api/transaction/bulk", {
             method: "POST",
             body: JSON.stringify(getAll.result),
             headers: {
-                Accept: "application/json, text/plain,*/*",
-                "Content-Type": "application/json"
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
             }
-        })
-        .then(response => response.json())
-        .then(() => {
+          })
+          .then(response => response.json())
+          .then(() => {
             const transaction = db.transaction(["pending"], "readwrite");
-
+    
             const store = transaction.objectStore("pending");
-
+    
             store.clear();
         });
     }
